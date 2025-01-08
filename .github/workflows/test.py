398ce7dab551c0776be8df8480b81aaa6de5a324
etalipov <talipov@skbkontur.ru>
2025-01-08 18:36:17 +0400

    Test github actions

---
+++ .github/workflows/test.py
@@ -0,0 +1,9 @@
+from os import environ
+
+
+print(environ.get("DIGEST"))
+print(environ.get("TEGS"))
+print(environ.get("labels"))
+print(environ.get("version"))
+print(environ.get("json"))
+print(environ.get("imageid"))
