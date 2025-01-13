bb7bbb38bf8f9dc32243269a7af0ad48ca39357d
etalipov <talipov@skbkontur.ru>
2025-01-13 16:54:49 +0400

    update

--- test.py
+++
@@ -1,9 +0,0 @@
-from os import environ
-
-
-print(environ.get("DIGEST"))
-print(environ.get("TEGS"))
-print(environ.get("labels"))
-print(environ.get("version"))
-print(environ.get("json"))
-print(environ.get("imageid"))
